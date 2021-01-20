bf037586c93697032f757472d3f2e96066e040de
sstraybird <sstraybird@163.com>
2021-01-20 17:19:17 +0800

    chapter 2

---
+++ src/wci/message/Message.java
@@ -0,0 +1,39 @@
+package wci.message;
+
+/**
+ * <h1>Message</h1>
+ *
+ * <p>Message format.</p>
+ */
+public class Message {
+    private MessageType type;
+    private Object body;
+    /**
+     * Constructor.
+     * @param type the message type.
+     * @param body the message body.
+     */
+    public Message(MessageType type, Object body)
+    {
+        this.type = type;
+        this.body = body;
+    }
+
+    /**
+     * Getter.
+     * @return the message type.
+     */
+    public MessageType getType()
+    {
+        return type;
+    }
+
+    /**
+     * Getter.
+     * @return the message body.
+     */
+    public Object getBody()
+    {
+        return body;
+    }
+}
